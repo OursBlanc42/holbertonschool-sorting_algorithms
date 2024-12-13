de4486690c3ab8eed1f376433380f568ed1eb224
OursBlanc <regniersimon@hotmail.com>
2024-12-13 09:56:56 +0100

    add a check to not print array if not necessary

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,11 +19,13 @@ void selection_sort(int *array, size_t size)
 	int cursor = 0;
 	int buffer = 0;
 	int play_again = 0;
+	int swapped = 0;
 
 	/* For each value in the array...*/
 	for (index = 0; index < (int)size; index++)
 	{
 		play_again = 0; 		/* reset checker */
+		swapped = 0;			/* reset swapped state */
 		index_mini = index;		/* suppose first digit at minimum */
 		mini =  array[index];	/* Set mini to first element */
 
@@ -34,22 +36,24 @@ void selection_sort(int *array, size_t size)
 			{
 				mini = array[cursor];
 				index_mini = cursor;
+				swapped = 1; 
 			}
 		}
 
-		/* Swap value and print result */
-		buffer = array[index];
-		array[index] = array[index_mini];
-		array[index_mini] = buffer;
-		print_array((const int *)array, size);
+		/* Swap value if necessary and print result */
+		if (swapped == 1)
+		{
+			buffer = array[index];
+			array[index] = array[index_mini];
+			array[index_mini] = buffer;
+			print_array((const int *)array, size);
+		}
 
 		/* check if the list is in right order to avoid useless traverse */
 		for (cursor = (index + 1) ; cursor < (int)size; cursor++)
 		{
 			if (array[cursor - 1] > array [cursor])
-			{
 				play_again = 1;
-			}
 		}
 
 		/* if is the right order : stop it, otherwise play again*/
